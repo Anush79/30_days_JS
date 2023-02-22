30efba79135e4725d244e250201b47e7c3c0cd01
Anushka J <66175237+Anush79@users.noreply.github.com>
2023-02-22 19:51:04 +0530

    Done with the functions of keys

--- drumKit_JS/script.js
+++ drumKit_JS/script.js
@@ -1,4 +1,20 @@
-window.addEventListener('keydown', (e)=>{
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-    console.log(audio)
-})
+const playBeat = (e) => {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //selects the audio
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); //selects the key
+  const keys = document.querySelectorAll(".key"); //selects all the keys
+
+  if (!audio) return; //stop the function from running all together
+  audio.currentTime = 0; //rewind to the start
+  audio.play(); //plays the audio
+  key.classList.add("playing"); //adds the class playing to the key
+
+  keys.forEach(
+    (key) => key.addEventListener("transitionend", removeEventListener) //adds the event listener to each key
+  );
+  function removeEventListener(e) {
+    //removes the class playing from the key
+    if (e.propertyName !== "transform") return; //skips it if it's not a transform
+    this.classList.remove("playing"); //removes the class playing
+  }
+};
+window.addEventListener("keydown", playBeat); //adds the event listener to the window
